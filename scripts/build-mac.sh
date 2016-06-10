465aa769e978446c4584b36d6b1f75e51b6f1a7d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-06-10 11:03:46 -0700

    always use unordered_map

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -4,12 +4,11 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-clang -g -Wall -DUNORDERED=1 -DLASZIPDLL_EXPORTS=1 -Isrc -lstdc++ src/*.cpp example/laszipdllexample.cpp -o lasz_tool
+clang -g -Wall -DLASZIPDLL_EXPORTS=1 -Isrc -lstdc++ src/*.cpp example/laszipdllexample.cpp -o lasz_tool
 
 # if gcc 5 is installed with: brew install gcc
-#/usr/local/bin/gcc-5 -g -Wall -DUNORDERED=1 -DLASZIPDLL_EXPORTS=1 -Isrc -lstdc++ src/*.cpp example/laszipdllexample.cpp -o lasz_tool_gcc
-
+#/usr/local/bin/gcc-5 -g -Wall -DLASZIPDLL_EXPORTS=1 -Isrc -lstdc++ src/*.cpp example/laszipdllexample.cpp -o lasz_tool_gcc
 
 # if latest clang is installed with: brew install llvm --HEAD
 # xcrun is needed to point to system c++ headers
-#xcrun /usr/local/opt/llvm/bin/clang -g -Wall -DUNORDERED=1 -DLASZIPDLL_EXPORTS=1 -Isrc -lstdc++ src/*.cpp example/laszipdllexample.cpp -o lasz_tool_clang
+#xcrun /usr/local/opt/llvm/bin/clang -g -Wall -DLASZIPDLL_EXPORTS=1 -Isrc -lstdc++ src/*.cpp example/laszipdllexample.cpp -o lasz_tool_clang
