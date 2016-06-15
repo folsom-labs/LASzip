42b2002c5fe0b1b2eba6dcc60e759520c79bf3a1
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-06-15 15:32:17 -0700

    define all the tags

--- geotiff.go
+++ geotiff.go
@@ -13,12 +13,9 @@ Tags have unique numeric ids. Their values can be: short (uint16),
 double (float64) or string.
 
 For convenience, we also put tag name as a string.
-*/
 
-const (
-	GTModelTypeGeoKey  = 1024
-	GTRasterTypeGeoKey = 1025
-)
+Spec: http://www.remotesensing.org/geotiff/spec/contents.html
+*/
 
 // GeoTagShort describes a tag whose value is short (uint16)
 type GeoTagShort struct {
@@ -80,14 +77,168 @@ type GeoKeyInfo struct {
 	ASCIIParams []byte
 }
 
+//
+// http://www.remotesensing.org/geotiff/spec/geotiff2.7.html#2.7
+// https://github.com/smanders/libgeotiff/blob/sdl_1_2_4/geokeys.inc
+const (
+	/* 6.2.1 GeoTIFF Configuration Keys */
+	GTModelTypeGeoKey  = 1024 /* Section 6.3.1.1 Codes       */
+	GTRasterTypeGeoKey = 1025 /* Section 6.3.1.2 Codes       */
+	GTCitationGeoKey   = 1026 /* documentation */
+
+	/* 6.2.2 Geographic CS Parameter Keys */
+	GeographicTypeGeoKey        = 2048 /* Section 6.3.2.1 Codes     */
+	GeogCitationGeoKey          = 2049 /* documentation             */
+	GeogGeodeticDatumGeoKey     = 2050 /* Section 6.3.2.2 Codes     */
+	GeogPrimeMeridianGeoKey     = 2051 /* Section 6.3.2.4 codes     */
+	GeogLinearUnitsGeoKey       = 2052 /* Section 6.3.1.3 Codes     */
+	GeogLinearUnitSizeGeoKey    = 2053 /* meters                    */
+	GeogAngularUnitsGeoKey      = 2054 /* Section 6.3.1.4 Codes     */
+	GeogAngularUnitSizeGeoKey   = 2055 /* radians                   */
+	GeogEllipsoidGeoKey         = 2056 /* Section 6.3.2.3 Codes     */
+	GeogSemiMajorAxisGeoKey     = 2057 /* GeogLinearUnits           */
+	GeogSemiMinorAxisGeoKey     = 2058 /* GeogLinearUnits           */
+	GeogInvFlatteningGeoKey     = 2059 /* ratio                     */
+	GeogAzimuthUnitsGeoKey      = 2060 /* Section 6.3.1.4 Codes     */
+	GeogPrimeMeridianLongGeoKey = 2061 /* GeoAngularUnit            */
+
+	/* 6.2.3 Projected CS Parameter Keys */
+	/*    Several keys have been renamed,*/
+	/*    and the deprecated names aliased for backward compatibility */
+	ProjectedCSTypeGeoKey          = 3072                       /* Section 6.3.3.1 codes */
+	PCSCitationGeoKey              = 3073                       /* documentation */
+	ProjectionGeoKey               = 3074                       /* Section 6.3.3.2 codes */
+	ProjCoordTransGeoKey           = 3075                       /* Section 6.3.3.3 codes */
+	ProjLinearUnitsGeoKey          = 3076                       /* Section 6.3.1.3 codes */
+	ProjLinearUnitSizeGeoKey       = 3077                       /* meters */
+	ProjStdParallel1GeoKey         = 3078                       /* GeogAngularUnit */
+	ProjStdParallelGeoKey          = ProjStdParallel1GeoKey     /* ** alias ** */
+	ProjStdParallel2GeoKey         = 3079                       /* GeogAngularUnit */
+	ProjNatOriginLongGeoKey        = 3080                       /* GeogAngularUnit */
+	ProjOriginLongGeoKey           = ProjNatOriginLongGeoKey    /* ** alias ** */
+	ProjNatOriginLatGeoKey         = 3081                       /* GeogAngularUnit */
+	ProjOriginLatGeoKey            = ProjNatOriginLatGeoKey     /* ** alias ** */
+	ProjFalseEastingGeoKey         = 3082                       /* ProjLinearUnits */
+	ProjFalseNorthingGeoKey        = 3083                       /* ProjLinearUnits */
+	ProjFalseOriginLongGeoKey      = 3084                       /* GeogAngularUnit */
+	ProjFalseOriginLatGeoKey       = 3085                       /* GeogAngularUnit */
+	ProjFalseOriginEastingGeoKey   = 3086                       /* ProjLinearUnits */
+	ProjFalseOriginNorthingGeoKey  = 3087                       /* ProjLinearUnits */
+	ProjCenterLongGeoKey           = 3088                       /* GeogAngularUnit */
+	ProjCenterLatGeoKey            = 3089                       /* GeogAngularUnit */
+	ProjCenterEastingGeoKey        = 3090                       /* ProjLinearUnits */
+	ProjCenterNorthingGeoKey       = 3091                       /* ProjLinearUnits */
+	ProjScaleAtNatOriginGeoKey     = 3092                       /* ratio */
+	ProjScaleAtOriginGeoKey        = ProjScaleAtNatOriginGeoKey /* ** alias ** */
+	ProjScaleAtCenterGeoKey        = 3093                       /* ratio */
+	ProjAzimuthAngleGeoKey         = 3094                       /* GeogAzimuthUnit */
+	ProjStraightVertPoleLongGeoKey = 3095                       /* GeogAngularUnit */
+	ProjRectifiedGridAngleGeoKey   = 3096                       /* GeogAngularUnit */
+
+	/* 6.2.4 Vertical CS Keys */
+	VerticalCSTypeGeoKey   = 4096 /* Section 6.3.4.1 codes   */
+	VerticalCitationGeoKey = 4097 /* documentation */
+	VerticalDatumGeoKey    = 4098 /* Section 6.3.4.2 codes   */
+	VerticalUnitsGeoKey    = 4099 /* Section 6.3.1 (.x) codes   */
+)
+
 // TagIDToName returns a string version of tagID value
 func TagIDToName(tagID int) string {
 
 	switch tagID {
+
 	case GTModelTypeGeoKey:
 		return "GTModelTypeGeoKey"
 	case GTRasterTypeGeoKey:
 		return "GTRasterTypeGeoKey"
+	case GTCitationGeoKey:
+		return "GTCitationGeoKey"
+	case GeographicTypeGeoKey:
+		return "GeographicTypeGeoKey"
+	case GeogCitationGeoKey:
+		return "GeogCitationGeoKey"
+	case GeogGeodeticDatumGeoKey:
+		return "GeogGeodeticDatumGeoKey"
+	case GeogPrimeMeridianGeoKey:
+		return "GeogPrimeMeridianGeoKey"
+	case GeogLinearUnitsGeoKey:
+		return "GeogLinearUnitsGeoKey"
+	case GeogLinearUnitSizeGeoKey:
+		return "GeogLinearUnitSizeGeoKey"
+	case GeogAngularUnitsGeoKey:
+		return "GeogAngularUnitsGeoKey"
+	case GeogAngularUnitSizeGeoKey:
+		return "GeogAngularUnitSizeGeoKey"
+	case GeogEllipsoidGeoKey:
+		return "GeogEllipsoidGeoKey"
+	case GeogSemiMajorAxisGeoKey:
+		return "GeogSemiMajorAxisGeoKey"
+	case GeogSemiMinorAxisGeoKey:
+		return "GeogSemiMinorAxisGeoKey"
+	case GeogInvFlatteningGeoKey:
+		return "GeogInvFlatteningGeoKey"
+	case GeogAzimuthUnitsGeoKey:
+		return "GeogAzimuthUnitsGeoKey"
+	case GeogPrimeMeridianLongGeoKey:
+		return "GeogPrimeMeridianLongGeoKey"
+	case ProjectedCSTypeGeoKey:
+		return "ProjectedCSTypeGeoKey"
+	case PCSCitationGeoKey:
+		return "PCSCitationGeoKey"
+	case ProjectionGeoKey:
+		return "ProjectionGeoKey"
+	case ProjCoordTransGeoKey:
+		return "ProjCoordTransGeoKey"
+	case ProjLinearUnitsGeoKey:
+		return "ProjLinearUnitsGeoKey"
+	case ProjLinearUnitSizeGeoKey:
+		return "ProjLinearUnitSizeGeoKey"
+	case ProjStdParallel1GeoKey:
+		return "ProjStdParallel1GeoKey"
+	case ProjStdParallel2GeoKey:
+		return "ProjStdParallel2GeoKey"
+	case ProjNatOriginLongGeoKey:
+		return "ProjNatOriginLongGeoKey"
+	case ProjNatOriginLatGeoKey:
+		return "ProjNatOriginLatGeoKey"
+	case ProjFalseEastingGeoKey:
+		return "ProjFalseEastingGeoKey"
+	case ProjFalseNorthingGeoKey:
+		return "ProjFalseNorthingGeoKey"
+	case ProjFalseOriginLongGeoKey:
+		return "ProjFalseOriginLongGeoKey"
+	case ProjFalseOriginLatGeoKey:
+		return "ProjFalseOriginLatGeoKey"
+	case ProjFalseOriginEastingGeoKey:
+		return "ProjFalseOriginEastingGeoKey"
+	case ProjFalseOriginNorthingGeoKey:
+		return "ProjFalseOriginNorthingGeoKey"
+	case ProjCenterLongGeoKey:
+		return "ProjCenterLongGeoKey"
+	case ProjCenterLatGeoKey:
+		return "ProjCenterLatGeoKey"
+	case ProjCenterEastingGeoKey:
+		return "ProjCenterEastingGeoKey"
+	case ProjCenterNorthingGeoKey:
+		return "ProjCenterNorthingGeoKey"
+	case ProjScaleAtNatOriginGeoKey:
+		return "ProjScaleAtNatOriginGeoKey"
+	case ProjScaleAtCenterGeoKey:
+		return "ProjScaleAtCenterGeoKey"
+	case ProjAzimuthAngleGeoKey:
+		return "ProjAzimuthAngleGeoKey"
+	case ProjStraightVertPoleLongGeoKey:
+		return "ProjStraightVertPoleLongGeoKey"
+	case ProjRectifiedGridAngleGeoKey:
+		return "ProjRectifiedGridAngleGeoKey"
+	case VerticalCSTypeGeoKey:
+		return "VerticalCSTypeGeoKey"
+	case VerticalCitationGeoKey:
+		return "VerticalCitationGeoKey"
+	case VerticalDatumGeoKey:
+		return "VerticalDatumGeoKey"
+	case VerticalUnitsGeoKey:
+		return "VerticalUnitsGeoKey"
 	default:
 		return fmt.Sprintf("unknown (%d)", tagID)
 	}
