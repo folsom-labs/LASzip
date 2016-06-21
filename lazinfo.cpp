c63eb6b9b28c0a9018f7a11d931a04f3631078d9
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2016-06-20 18:02:31 -0700

    calc min x/y/z from .laz points

--- lazinfo.cpp
+++ lazinfo.cpp
@@ -34,7 +34,7 @@ static void fatal(void * laszip=nullptr)
 static void unlaz(char *file_name_in) {
   printf("file: %s\n", file_name_in);
 
-  void * laszip_reader;
+  void * laszip_reader = nullptr;
   if (laszip_create(&laszip_reader))
   {
     fprintf(stderr,"DLL ERROR: creating laszip reader\n");
@@ -50,6 +50,62 @@ static void unlaz(char *file_name_in) {
 
   fprintf(stderr,"file '%s' is %scompressed\n", file_name_in, (is_compressed ? "" : "un"));
 
+  laszip_header* header;
+  if (laszip_get_header_pointer(laszip_reader, &header))
+  {
+    fprintf(stderr,"DLL ERROR: getting header pointer from laszip reader\n");
+    fatal(laszip_reader);
+  }
+
+  uint64_t npoints = (header->number_of_point_records ? header->number_of_point_records : header->extended_number_of_point_records);
+
+  fprintf(stderr,"file '%s' contains %lld points\n", file_name_in, npoints);
+
+  // this is where point data is after reading
+  laszip_point* point;
+
+  int32_t maxX = 0, maxY = 0, maxZ = 0;
+  int32_t minX = 0, minY = 0, minZ = 0;
+
+  if (laszip_get_point_pointer(laszip_reader, &point))
+  {
+    fprintf(stderr,"DLL ERROR: getting point pointer from laszip reader\n");
+    fatal(laszip_reader);
+  }
+
+  for (uint64_t n = 0; n < npoints; n++) {
+    if (laszip_read_point(laszip_reader))
+    {
+      fprintf(stderr,"DLL ERROR: reading point %lld\n", n);
+      fatal(laszip_reader);
+    }
+    if (n == 0) {
+      minX = maxX = point->X;
+      minY = maxY = point->Y;
+      minZ = maxZ = point->Z;
+    } else {
+      if (point->X > maxX) {
+        maxX = point->X;
+      }
+      if (point->Y > maxY) {
+        maxY = point->Y;
+      }
+      if (point->Z > maxZ) {
+        maxZ = point->Z;
+      }
+
+      if (point->X < minX) {
+        minX = point->X;
+      }
+      if (point->Y < minY) {
+        minY = point->Y;
+      }
+      if (point->Z > minZ) {
+        minZ = point->Z;
+      }
+    }
+  }
+
   if (laszip_close_reader(laszip_reader))
   {
     fprintf(stderr,"DLL ERROR: closing laszip reader\n");
@@ -62,6 +118,9 @@ static void unlaz(char *file_name_in) {
     fatal();
   }
 
+  fprintf(stdout, "minX: %d maxX: %d\n", (int)minX, (int)maxX);
+  fprintf(stdout, "minY: %d maxY: %d\n", (int)minY, (int)maxY);
+  fprintf(stdout, "minZ: %d maxZ: %d\n", (int)minZ, (int)maxZ);
 }
 
 int main(int argc, char *argv[])
--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 clang -std=c++11 -g -Wall -DLASZIPDLL_EXPORTS=1 -Isrc -lstdc++ src/*.cpp lazinfo.cpp -o lazinfo
 
-clang -std=c++11 -g -Wall -DLASZIPDLL_EXPORTS=1 -Isrc -lstdc++ src/*.cpp example/laszipdllexample.cpp example/examples.cpp -o lasz_tool
+# clang -std=c++11 -g -Wall -DLASZIPDLL_EXPORTS=1 -Isrc -lstdc++ src/*.cpp example/laszipdllexample.cpp example/examples.cpp -o lasz_tool
 
 # if gcc 5 is installed with: brew install gcc
 #/usr/local/bin/gcc-5 -g -Wall -DLASZIPDLL_EXPORTS=1 -Isrc -lstdc++ src/*.cpp unlaz.cpp -o lasz_tool_gcc
